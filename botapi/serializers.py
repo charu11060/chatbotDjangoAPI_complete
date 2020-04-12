b356feb59f61121f594a3dbed811f85e25cc4da6
charu11060 <33230302+charu11060@users.noreply.github.com>
2020-04-13 02:27:54 +0530

    Delete serializers.py

--- botapi/serializers.py
+++
@@ -1,8 +0,0 @@
-# serializers.py
-from rest_framework import serializers
-
-from .models import Chat
-
-class ChatSerializer(serializers.HyperlinkedModelSerializer):
-    q = serializers.CharField(max_length=100)
-    a = serializers.CharField(max_length=100)
