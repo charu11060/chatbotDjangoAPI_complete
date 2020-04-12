8c7a7aa5942eda731e138caa799118c09e7d1a28
charu11060 <33230302+charu11060@users.noreply.github.com>
2020-04-13 02:04:39 +0530

    Update models.py

--- botapi/models.py
+++ botapi/models.py
@@ -1,11 +1,1 @@
 # models.py
-from django.db import models
-
-# Create your models here.
-
-class Chat(models.Model):
-    q = models.CharField(max_length=260)
-    a = models.CharField(max_length=260)
-    tag = models.CharField(max_length=80)
-    def __str__(self):
-        return self.q
